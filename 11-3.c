916f66bc23049ac7891c986b98d423aaed29d3cb
yuke_1412 <yuke_1412@163.com>
2017-11-23 08:59:19 +0800

    add 11-3.c file

---
+++ 11-3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+	char a[] = "I am a programmer", b[20];
+	int i;
+	for (i = 0; *(a+i)!='\0';i++)
+	{
+		*(b+i) = *(a+i);
+	}
+	*(b+i) = '\0';
+
+	printf("string a is %s\n", a);
+	printf("string b is %s\n", b);
+
+	for (i = 0; b[i]!='\0';i++)
+	{
+		printf("%c",b[i]);
+	}
+	printf("\n");
+	return 0;
+}
+

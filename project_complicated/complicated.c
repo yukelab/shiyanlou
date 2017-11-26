f9785d3b840ae90605f9706fe082af7b3f8e449e
yuke_1412 <yuke_1412@163.com>
2017-11-26 19:51:06 +0800

    add makefile project

---
+++ project_complicated/complicated.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "complicated.h"
+void complicated(void)
+{
+	printf("This is a %s project!\n", PROJECT_NAME);
+}
---
+++ project_complicated/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "complicated.h"
+int main()
+{
+	printf("%s\n", HELLO_STRING);
+	complicated();
+	return 0;
+}

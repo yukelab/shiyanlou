49957bb61f98b2fa82bede4bb21951d5799a7e92
yukelab <yuke_1412@163.com>
2017-12-01 15:37:29 +0800

    redistribute folders

--- c_files/copy.c
+++ c_files/copy.c
@@ -5,15 +5,13 @@ int main(int argc, char *argv[])
 {
 	FILE *fp1, *fp2;
     char ch;
-    printf ("input argv[1]:%s", argv[1]);
-    printf ("input argv[2]:%s", argv[2]);
 	if ((fp1 = fopen(argv[1],"r"))==NULL)
 	{
 		printf("error, copy origin file is  not exist!");
 		exit(0);
 	}
 	
-	if ((fp2 = fopen(argv[2],"r"))==NULL)
+	if ((fp2 = fopen(argv[2],"r"))!=NULL)
 	{
 		printf("error, copy dest file is exist!");
 		exit(0);

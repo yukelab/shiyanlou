41221e4b515d00cf550996fc9d41f7dc8ee6f24a
yuke_1412 <yuke_1412@163.com>
2017-12-12 19:05:15 +0800

    math_proc success

--- sql/ch3_grade.sql
+++ sql/ch3_grade.sql
@@ -8,3 +8,18 @@ end
 delimiter ; 
 
 update mark set score = score + 3 where sid = (select sid from student where sname = 'Tom') and cid = (select cid from course where cname = 'chemistry');
+
+
+delimiter |
+create procedure math_proc () 
+begin
+select 'math', sname, score 
+from  student, mark 
+where mark.cid = (select cid from course where cname = 'math') and student.sid = mark.sid
+order by score desc;
+end
+| 
+delimiter ; 
+
+call math_proc();
+

ad04eb02f830954d7b97e8a375371fba4fe110b9
yuke_1412 <yuke_1412@163.com>
2017-12-12 17:36:29 +0800

    trigger_modify success

--- sql/ch3_grade.sql
+++ sql/ch3_grade.sql
@@ -2,8 +2,9 @@
 delimiter |
 create trigger trigger_modify before update on mark
 for each row begin
-insert into modifymark value (NULL,new.sid, new.cid, new.score,curtime());
+insert into modifymark value (NULL,new.sid, new.cid, new.score,now());
 end
 | 
 delimiter ; 
+
 update mark set score = score + 3 where sid = (select sid from student where sname = 'Tom') and cid = (select cid from course where cname = 'chemistry');

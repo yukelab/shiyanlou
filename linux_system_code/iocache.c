e8240539fad813b63c4cfeebe7865346695baa45
yuke_1412 <yuke_1412@163.com>
2017-12-19 10:13:23 +0800

    add echoall and exithandle.c file

---
+++ linux_system_code/iocache.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+int main(void)
+{
+    printf("hello\n");
+
+    sleep(100);
+    return 0;
+}
---
+++ linux_system_code/wait.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <wait.h>
+
+void pr_exit(int status);
+
+int main(void)
+{
+    pid_t pid; 
+    int	status;
+
+    if ((pid = fork()) < 0)
+    {
+	perror("fork");
+	exit(-1);
+    }
+    else if (pid == 0)
+    {
+	sleep(1);
+	printf("inchild\n");
+	exit(101);
+    }
+
+    if (wait(&status) != pid)
+    {
+	perror("wait");
+	exit(-2);
+    }
+    printf("in parent\n");
+    pr_exit(status);
+
+    if ((pid = fork()) < 0)
+    {
+	perror("fork");
+	exit(-1);
+    }
+    else if (pid == 0)
+    {
+	abort();
+    }
+
+    if (wait(&status) != pid)
+    {
+	perror("wait");
+	exit(-2);
+    }
+    pr_exit(status);
+
+    if ((pid = fork()) < 0)
+    {
+	perror("fork");
+	exit(-1);
+    }
+    else if (pid == 0)
+    {
+	status /= 0;
+    }
+    if (wait(&status) != pid)
+    {
+	perror("wait");
+	exit(-1);
+    }
+    pr_exit(status);
+    exit(0);
+}
+
+void pr_exit(int status)
+{
+    if (WIFEXITED(status))
+    {
+	printf("normally termination, low-order 8 bit of exit status = %d\n",WEXITSTATUS(status));
+    }
+    else if(WIFSIGNALED(status))
+    {
+	printf("abnormally termination, signal number = %d\n",WTERMSIG(status));
+    }
+}
